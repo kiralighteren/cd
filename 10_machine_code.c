8d2cea77744d4c688feb8a67b84e5c119490e485
kiralighteren <orewaerenjaegar@gmail.com>
2025-11-03 01:52:21 +0530

    Create 10_machine_code.c

---
+++ 10_machine_code.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Instruction {
+    char result[10];
+    char arg1[10];
+    char op[5];
+    char arg2[10];
+};
+
+int main() {
+    int n;
+    printf("Enter number of intermediate code instructions: ");
+    scanf("%d", &n);
+
+    struct Instruction ic[20];
+    printf("\nEnter intermediate code in the format (result = arg1 op arg2):\n");
+    for (int i = 0; i < n; i++) {
+        printf("Instruction %d: ", i + 1);
+        scanf("%s = %s %s %s", ic[i].result, ic[i].arg1, ic[i].op, ic[i].arg2);
+    }
+
+    printf("\n=== Generated Machine Code ===\n");
+    for (int i = 0; i < n; i++) {
+        printf("LOAD %s\n", ic[i].arg1);
+        if (strcmp(ic[i].op, "+") == 0)
+            printf("ADD %s\n", ic[i].arg2);
+        else if (strcmp(ic[i].op, "-") == 0)
+            printf("SUB %s\n", ic[i].arg2);
+        else if (strcmp(ic[i].op, "*") == 0)
+            printf("MUL %s\n", ic[i].arg2);
+        else if (strcmp(ic[i].op, "/") == 0)
+            printf("DIV %s\n", ic[i].arg2);
+        else
+            printf("NOP // Unknown operation\n");
+        printf("STORE %s\n\n", ic[i].result);
+    }
+    return 0;
+}
